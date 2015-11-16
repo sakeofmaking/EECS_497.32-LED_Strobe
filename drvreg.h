92f326731e4e17398f0c12818077dc6cd798abfa
Nic La <sakeofmaking@gmail.com>
2015-11-15 17:23:29 -0800

    Update drvreg.h

--- drvreg.h
+++ drvreg.h
@@ -1,25 +1,28 @@
-#ifndef DRVREG_H
+/* Port A */
+#define PINA 0x20
+#define DDRA 0x21
+#define PORTA 0x22
 
-#define DRVREG_H
-#define PINA  0x20  //Input Pins, Port A
-#define DDRA  0x21 //Data Direction Register, Port A
-#define PORTA 0x22 //Data Register, Port A
+/* Port B */
+#define PINB 0x23
+#define DDRB 0x24
+#define PORTB 0x25
 
-#define PINB  0x23  //Input Pins, Port B
-#define DDRB  0x24 //Data Direction Register, Port B
-#define PORTB 0x25 //Data Register, Port B
-
-
-#define GIMSK 0x5B //General Interrupt Mask register
-#define GIFR 0x5A //General Interrupt Flag Register
-#define TIMSK0 0x6E //Timer/Counter Interrupt Mask register
-#define TIFR0 0x35 //Timer/Counter Interrupt Flag register
-#define MCUCR 0x55 //MCU general Control Register
-#define TCCR0A 0x44 //Timer/Counter0 Control Register
-#define TCCR0B 0x45 //Timer/Counter0 Control Register
-#define OCR0A  0x47 // Output Compare Register A
-#define TCNT0 0x46 //Timer/Counter0 (8-bit)
-
- 
-#endif
- 
+/* Timer 0 */
+#define OCR0A 0x47
+#define OCR0B 0x48
+#define TCCR0A 0x44
+#define COM0A1 7
+#define COM0A0 6
+#define COM0B1 5
+#define COM0B0 4
+#define WGM01 1
+#define WGM00 0
+#define TCCR0B 0x45
+#define CS02 2 
+#define CS00 0
+#define TCNT0 0x46
+#define TIFR0 0x35
+#define OCF0B 2
+#define OCF0A 1
+#define TOV0 0
